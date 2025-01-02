0e61192333274b406eccc2974865978eabbe29c2
chi tseng <vitus.tseng@gmail.com>
2025-01-02 22:44:33 +0800

    remove never use library

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Runtime.CompilerServices;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace personal_note
 {
