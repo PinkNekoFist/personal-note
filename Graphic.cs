5a12b5f11dd0fada6050079af5477e5f00b438ed
chi tseng <vitus.tseng@gmail.com>
2025-01-02 16:27:58 +0800

    add pie chart

--- Graphic.cs
+++ Graphic.cs
@@ -16,10 +16,11 @@ namespace personal_note
         public Graphic(List<float> stars)
         {
             InitializeComponent();
-            ShowGraph(stars);
+            ShowLineChart(stars);
+            ShowPieChart(stars);
         }
 
-        private void ShowGraph(List<float> stars)
+        private void ShowLineChart(List<float> stars)
         {
             // Create a new Chart
             Chart lineChart = new Chart();
@@ -54,6 +55,67 @@ namespace personal_note
 
             // Add the Chart to the Form
             this.Controls.Add(lineChart);
+        }
+
+        private void ShowPieChart(List<float> stars)
+        {
+            // Convert stars to int
+            List<int> intStars = stars.Select(s => (int)s).ToList();
+
+            // Create a new Chart
+            // Create a new Chart
+            Chart pieChart = new Chart();
+            pieChart.Size = new Size(600, 400);
+            pieChart.Location = new Point(700, 10);
+
+            // Create a ChartArea
+            ChartArea chartArea = new ChartArea();
+            pieChart.ChartAreas.Add(chartArea);
+
+            // Create a Series and add data points
+            Series series = new Series
+            {
+                Name = "DiaryData",
+                IsVisibleInLegend = true,
+                ChartType = SeriesChartType.Pie
+            };
+
+            int[] count = new int[6];
+            for (int i = 0; i < intStars.Count; i++)
+            {
+                count[intStars[i]]++;
+            }
+
+            // Example data points
+            series.Points.Add(new DataPoint(0, count[0]) { LegendText = "zero" });
+            series.Points.Add(new DataPoint(0, count[1]) { LegendText = "one" });
+            series.Points.Add(new DataPoint(0, count[2]) { LegendText = "two" });
+            series.Points.Add(new DataPoint(0, count[3]) { LegendText = "three" });
+            series.Points.Add(new DataPoint(0, count[4]) { LegendText = "four" });
+            series.Points.Add(new DataPoint(0, count[5]) { LegendText = "five" });
+
+            // add a color/lengend to each data point
+            series.Points[0].Color = Color.Gray;
+            series.Points[1].Color = Color.Orange;
+            series.Points[2].Color = Color.Yellow;
+            series.Points[3].Color = Color.Green;
+            series.Points[4].Color = Color.Blue;
+            series.Points[5].Color = Color.Purple;
+
+            // show color meaning in the legend
+            pieChart.Legends.Add(new Legend("Legend1"));
+            pieChart.Legends[0].Docking = Docking.Bottom;
+            pieChart.Legends[0].Alignment = StringAlignment.Center;
+            pieChart.Legends[0].Title = "心情指數";
+            pieChart.Legends[0].TitleBackColor = Color.White;
+            pieChart.Legends[0].TitleForeColor = Color.Black;
+
+
+
+            pieChart.Series.Add(series);
+
+            // Add the Chart to the Form
+            this.Controls.Add(pieChart);
         }
     }
 }
